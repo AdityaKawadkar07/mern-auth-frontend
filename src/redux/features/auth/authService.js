f03b1c0f04df2d1b9c33fde73d946b171b889546
AdityaKawadkar07 <adityakawadkar7@gmail.com>
2024-04-02 01:10:31 +0530

    fifth

--- src/redux/features/auth/authService.js
+++ src/redux/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL="https://authz-app-api-rvbc.onrender.com"
+const BACKEND_URL=process.env.REACT_APP_BACKEND_URL;
 export const API_URL = `${BACKEND_URL}/api/users/`;
 
 // Validate email
